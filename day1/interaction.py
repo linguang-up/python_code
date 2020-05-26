08609ab91af22395c93e30e9a53bcded2e42d6dc
T400 <yinxiandelan@163.com>
2020-05-26 22:08:10 +0800

    update interaction

--- day1/interaction.py
+++ day1/interaction.py
@@ -3,8 +3,16 @@
 #密文密码,getpass模块在pycharm中无法正常执行，需要在命令行中执行
 
 import getpass
+db_name = "linguang"
+db_passwd = "abc123.."
+
 
 username = input("username:")
-password = getpass.getpass("password:")
+# password = getpass.getpass("password:")
+password = input("password:")
 
-print("username",password)
+if username == db_name and password == db_passwd :
+    print("用户：%s，登录成功" %(username))
+
+else :
+    print("Invalid account password!")
