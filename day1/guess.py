40d49baddc00f7e49a2085348e16702c7b503b2b
T400 <yinxiandelan@163.com>
2020-05-26 21:51:01 +0800

    update guess

--- day1/guess.py
+++ day1/guess.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # Author:Lin Guang
+
+
+age_of_linguang = 30
+
+guess_age = int(input("guess age:"))
+
+if guess_age == age_of_linguang :
+    print("you got it!")
+
+elif guess_age > age_of_linguang :
+    print("you must guess smaller!")
+
+elif guess_age < age_of_linguang :
+    print("you must guess bigger!!")
+
+else:
+    print("pls input type int!!!")
