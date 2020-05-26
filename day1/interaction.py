896a32faa2840b5675c63a2985cbb16bba7fcbfd
T460 <yinxiandelan@163.com>
2020-05-26 17:06:32 +0800

    new interaction

--- day1/interaction.py
+++ day1/interaction.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # Author:Lin Guang
+#密文密码,getpass模块在pycharm中无法正常执行，需要在命令行中执行
 
+import getpass
+
+username = input("username:")
+password = getpass.getpass("password:")
+
+print("username",password)
